83c9ffd18b9f59065a90d833789c46b347fa45b6
AUXIN123 <hamzarafsan2018@gmail.com>
2025-11-14 19:54:40 +0600

    fix

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -16,9 +16,9 @@ const localConfig = {
 };
 
 // ✅ Use environment variables if available (for production)
-const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+const firebaseConfig = process.env.FIREBASE_API_KEY
   ? {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      apiKey: process.env.FIREBASE_API_KEY,
       authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
